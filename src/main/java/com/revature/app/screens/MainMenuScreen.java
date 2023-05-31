89489a03c9d0e75721743b3274a369089b299f58
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-05-30 22:05:38 -0400

    fix nav issue

--- src/main/java/com/revature/app/screens/MainMenuScreen.java
+++ src/main/java/com/revature/app/screens/MainMenuScreen.java
@@ -27,47 +27,50 @@ public class MainMenuScreen implements IScreen{
     @Override
     public void start(Scanner scan) {
         log.info("navigated to main menu screen");
-        while(true) {
-            clearScreen();
-            System.out.println("Welcome to Paimon's Bargains!");
-            System.out.println("Please select from the menu:\n");
-
-            System.out.println("[1] View Cart | [2] View Purchase History | [3] Browse Inventory | [4] View Your Reviews | [5] Logout\n");
-
-            String input = scan.nextLine();
-            switch(input) {
-                case "1":
-                    log.info("navigating to cart screen");
-                    session.getScreenHistory().push("/menu");
-                    router.navigate("/cart", scan);
-                    break;
-                case "2":
-                    log.info("navigating to purchase history screen");
-                    session.getScreenHistory().push("/menu");
-                    router.navigate("/history", scan);
-                    break;
-                case "3":
-                    log.info("navigating to browse products screen");
-                    session.getScreenHistory().push("/menu");
-                    router.navigate("/browse", scan);
-                    break;
-                case "4":
-                    log.info("navigating to User reviews screen");
-                    session.getScreenHistory().push("/menu");
-                    router.navigate("/review", scan);
-                    break;
-                case "5":
-                    log.info("Log out, navigating to home screen");
-                    session.getScreenHistory().push("/menu");
-                    router.navigate("/home", scan);
-                    break;
-                default:
-                    log.warn("invalid input");
-                    System.out.print("Invalid option. Press any key to continue... ");
-                    scan.nextLine();
-                    break;
+        exit:{
+            while(true) {
+                clearScreen();
+                System.out.println("Welcome to Paimon's Bargains!");
+                System.out.println("Please select from the menu:\n");
+    
+                System.out.println("[1] View Cart | [2] View Purchase History | [3] Browse Inventory | [4] View Your Reviews | [5] Logout\n");
+    
+                String input = scan.nextLine();
+                switch(input) {
+                    case "1":
+                        log.info("navigating to cart screen");
+                        session.getScreenHistory().push("/menu");
+                        router.navigate("/cart", scan);
+                        break;
+                    case "2":
+                        log.info("navigating to purchase history screen");
+                        session.getScreenHistory().push("/menu");
+                        router.navigate("/history", scan);
+                        break;
+                    case "3":
+                        log.info("navigating to browse products screen");
+                        session.getScreenHistory().push("/menu");
+                        router.navigate("/browse", scan);
+                        break;
+                    case "4":
+                        log.info("navigating to User reviews screen");
+                        session.getScreenHistory().push("/menu");
+                        router.navigate("/review", scan);
+                        break;
+                    case "5":
+                        log.info("Log out, navigating to home screen");
+                        session.getScreenHistory().push("/menu");
+                        router.navigate("/home", scan);
+                        break exit;
+                    default:
+                        log.warn("invalid input");
+                        System.out.print("Invalid option. Press any key to continue... ");
+                        scan.nextLine();
+                        break;
+                }
             }
         }
+        
     }
 
     /**
